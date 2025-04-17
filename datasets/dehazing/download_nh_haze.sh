976293b318fb9c952e5eb6dde00dae15c5410b7b
Sachin Chaudhary <122465638+Sachin1987123@users.noreply.github.com>
2025-04-17 11:29:10 +0530

    Initial

---
+++ datasets/dehazing/download_nh_haze.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://data.vision.ee.ethz.ch/cvl/ntire20/nh-haze/NH-HAZE.zip -P datasets/dehazing/NH-Haze/
+unzip datasets/dehazing/NH-Haze/NH-HAZE.zip -d datasets/dehazing/NH-Haze/
---
+++ datasets/deraining/download_raindrop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gdown https://drive.google.com/uc?id=1e7R76s6vwUJxILOcAsthgDLPSnOrQ49K -O datasets/deraining/RainDrop.zip
+unzip datasets/deraining/RainDrop.zip -d datasets/deraining/RainDrop/
